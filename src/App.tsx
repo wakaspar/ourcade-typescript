6f3145c1b6a6258e2ad7ebdd0b9afdf796db1799
wakaspar <wakaspar@gmail.com>
2020-06-11 10:49:13 -0500

    reorganizes depenedencies in package.json, adds history to @types, edits App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+import icon from "./pins-icon.png"
+
+import CreateScore from "./components/create-score.component";
+import EditScore from "./components/edit-score.component";
+import DeleteScore from "./components/delete-score.component";
+import ScoresList from "./components/scores-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+           <a className="navbar-brand" href="/" target="_blank">
+             <img src={icon} alt="Ourcade React" />
+           </a>
+           <Link to="/" className="navbar-brand">Ourcade-TypeScript</Link>
+           <div className="collpase navbar-collapse">
+             <ul className="navbar-nav mr-auto">
+               <li className="navbar-item">
+                 <Link to="/" className="nav-link">Scoreboard</Link>
+               </li>
+               <li className="navbar-item">
+                 <Link to="/create" className="nav-link">Create Score</Link>
+               </li>
+             </ul>
+           </div>
+         </nav>
+         <br/>
+         {/* Route Definitons - NOTE: keep inside bootstrap container  */}
+         <Route path ="/" exact component={ScoresList} />
+         <Route path ="/edit/:id" component={EditScore} />
+         <Route path ="/create" component={CreateScore} />
+         <Route path ="/delete/:id" component={DeleteScore} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
